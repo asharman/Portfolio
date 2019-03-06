2114df78a365904b9bcdf758a8cf56f20c807363
Austin Sharman <asharman22@gmail.com>
2019-03-06 12:54:14 -0500

    Added focus transitions to the project thumbnails

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,13 +1,42 @@
+// Scroll down to project section
+
 const viewWorkButton = document.getElementById("view-work");
 const viewWorkArrow = document.getElementById("view-work-arrow");
 const projectPage = document.getElementById('project-page');
 
-viewWorkButton.addEventListener('click', scrollToProjects);
-viewWorkArrow.addEventListener('click', scrollToProjects);
-
-function scrollToProjects(e) {
+scrollToProjects = e => {
   const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
 
   e.preventDefault();
   window.scrollBy({ top: distanceToTop(projectPage), left: 0, behavior: 'smooth' });
 }
+
+viewWorkButton.addEventListener('click', scrollToProjects);
+viewWorkArrow.addEventListener('click', scrollToProjects);
+
+
+// Have a short summary and link to more info pop up when you click on thumbnails
+showInfo = e => {
+  e.preventDefault();
+  const targetInfo = e.target.attributes.info.value;
+
+  const infoElement = document.getElementById(targetInfo);
+
+  infoElement.classList.remove('hidden');
+}
+
+hideInfo = e => {
+  e.preventDefault();
+  const targetInfo = e.target.attributes.info.value;
+
+  const infoElement = document.getElementById(targetInfo);
+
+  infoElement.classList.add('hidden');
+}
+
+let thumbnails = document.querySelectorAll('.thumbnail');
+
+thumbnails.forEach(element => {
+  element.addEventListener('focus', showInfo);
+  element.addEventListener('blur', hideInfo);
+});
