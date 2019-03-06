604d2c624b84aa72780c9e4be4f65e6e629449ad
Austin Sharman <asharman22@gmail.com>
2019-03-06 09:20:52 -0500

    Added smooth scroll to projects page when View Work is clicked

---
+++ Javascript/index.js
@@ -0,0 +1,14 @@
+const viewWorkButton = document.getElementById("view-work");
+const viewWorkArrow = document.getElementById("view-work-arrow");
+const projectPage = document.getElementById('project-page');
+
+viewWorkButton.addEventListener('click', scrollToProjects);
+viewWorkArrow.addEventListener('click', scrollToProjects);
+
+function scrollToProjects(e) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+
+  e.preventDefault();
+  window.scrollBy({ top: distanceToTop(projectPage), left: 0, behavior: 'smooth' });
+
+}
